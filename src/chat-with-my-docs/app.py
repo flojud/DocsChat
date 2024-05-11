fdbb8d8a885b7a5a3b0a1ff602682d3a24878447
Florian Jud <florian.jud@mercedes-benz.com>
2024-05-11 20:17:13 +0200

    Yet another streamlit package version

--- src/chat-with-my-docs/app.py
+++ src/chat-with-my-docs/app.py
@@ -1,8 +1,11 @@
 import os
 
-from streamlit.web import cli
+from streamlit import config as _config
+from streamlit.web.bootstrap import run
 
 if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, "main.py")
-    cli.main_run([filename])
+
+    _config.set_option("server.headless", True)
+    run(filename, args=[], flag_options=[], is_hello=False)
