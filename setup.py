ab2c0141010ac2ff72cf98fa2bbea98086aea676
GitHub Action <dev.flojud@gmail.com>
2024-05-11 15:28:17 +0000

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="Chat_with_my_docs",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=[],
