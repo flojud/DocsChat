411fc0aeca1651b0a5c80e5bff206563e9da9067
GitHub Action <dev.flojud@gmail.com>
2024-05-12 14:14:09 +0000

    Bump version: 0.14.0 → 0.15.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="DocChat",
-    version="0.14.0",
+    version="0.15.0",
     package_dir={"": "src"},
     packages=["docchat"],
     author="flojud",
